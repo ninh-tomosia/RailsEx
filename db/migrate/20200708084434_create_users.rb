8b5cce64664786aaea1192432fe6f5ac3e77bc17
Le Van Ninh <tt.ninh.le@tomosia.com>
2020-07-08 15:53:01 +0700

    create data from db.user

--- db/migrate/20200708084434_create_users.rb
+++ db/migrate/20200708084434_create_users.rb
@@ -1,6 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+      t.string :phone
+      t.string :address
+      t.string :role
 
       t.timestamps
     end
