1f0ae808b549deb4bd8356bfafe417e548531b19
Le Van Ninh <tt.ninh.le@tomosia.com>
2020-07-08 16:58:50 +0700

    create view home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  get "home", to: "home#index"
+  resources :home
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
